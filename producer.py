48b65c04a6a397763a33f4b1401582d3e3589716
Vinh Phan <pvphan90@gmail.com>
2023-10-11 00:40:13 -0700

    multiple batches

--- producer.py
+++ producer.py
@@ -11,17 +11,6 @@ def main():
     if os.path.exists(addressing.handle):
         os.remove(addressing.handle)
 
-    num_points = 328 * 225
-    x = np.random.uniform(0, 1000, num_points).astype(np.float32)
-    y = np.random.uniform(0, 1000, num_points).astype(np.float32)
-    z = np.random.uniform(0, 1000, num_points).astype(np.float32)
-    values = np.random.randint(0, 256, num_points, dtype=np.uint8)
-
-    batch = pa.record_batch(
-        [x, y, z, values],
-        names=["x", "y", "z", "values"],
-    )
-
     # Set up a socket server for IPC communication
     with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as server_socket:
         server_socket.bind(addressing.handle)
@@ -33,12 +22,29 @@ def main():
         connection, client_address = server_socket.accept()
 
         # Send the Arrow Array through IPC
-        sink = connection.makefile("wb", int(1.10 * batch.get_total_buffer_size()))
+        batch = createBatch()
+        num_batches = 5
+        sink = connection.makefile("wb", int(1.10 * num_batches * batch.get_total_buffer_size()))
         with pa.ipc.new_stream(sink, batch.schema) as writer:
-            writer.write_batch(batch)
+            for _ in range(num_batches):
+                batch = createBatch()
+                writer.write_batch(batch)
         connection.close()
 
         print(f"IPC data sent to consumer at {client_address}")
+
+
+def createBatch():
+    num_points = 328 * 225
+    x = np.random.uniform(0, 1000, num_points).astype(np.float32)
+    y = np.random.uniform(0, 1000, num_points).astype(np.float32)
+    z = np.random.uniform(0, 1000, num_points).astype(np.float32)
+    values = np.random.randint(0, 256, num_points, dtype=np.uint8)
+    batch = pa.record_batch(
+        [x, y, z, values],
+        names=["x", "y", "z", "values"],
+    )
+    return batch
 
 
 if __name__ == "__main__":
